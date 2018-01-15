eb3142b3a85bdc08d92541454164e522f9ee5ea4
liumengchn <liumc1993520@live.com>
2018-01-15 22:09:43 +0800

    注册页面的添加

---
+++ Clinic_Doctors/Modules/Patient/ClinicLogin/Controller/PatientLoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PatientLoginViewController.swift
+//  Clinic_Doctors
+//
+//  Created by liumenchen on 2018/1/15.
+//  Copyright © 2018年 Mengchen Liu. All rights reserved.
+//
+
+import UIKit
+
+class PatientLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Clinic_Doctors/Modules/Patient/ClinicLogin/Service/PatientAccountServer.swift
@@ -0,0 +1,9 @@
+//
+//  PatientAccountServer.swift
+//  Clinic_Doctors
+//
+//  Created by liumenchen on 2018/1/15.
+//  Copyright © 2018年 Mengchen Liu. All rights reserved.
+//
+
+import Foundation
